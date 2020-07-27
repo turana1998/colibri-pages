d016904edd550b3a7513d2dfdfb3f85c1d543153
turana1998 <turana221298@gmail.com>
2020-07-27 10:57:23 +0400

    up

---
+++ style/mystyle/js/my_style.js
@@ -0,0 +1,72 @@
+// $(".navbar-toggler").click(function () {
+
+//   // var element = document.querySelector("#nav_togle");
+//   // console.log(document.querySelector("#nav_togle"));
+//   if (document.querySelector("#nav_togle").classList.contains("collapsed")) {
+//     document.getElementById("footer").style.display = "none"
+//     document.getElementById("navmakenone").style.display = "none"
+//     setTimeout(function () {
+//       document.getElementById("overlay").style.display = "block";
+//     }, 340)
+//   } else {
+//     // alert()
+//     document.getElementById("navmakenone").style.display = "block"
+//     document.getElementById("footer").style.display = "block"
+//     setTimeout(function () {
+//       document.getElementById("overlay").style.display = "none";
+//     }, 20)
+//   }
+// });
+// $(".collapsed").click(function () {
+//     document.getElementById("overlay").style.display = "block";
+// });
+
+
+// function on() {
+
+// }
+
+// function off() {
+//     document.getElementById("overlay").style.display = "none";
+// }
+$(document).on('ready', function () {
+
+  $('.field').on('focus', function () {
+    $('body').addClass('is-focus');
+  });
+
+  $('.field').on('blur', function () {
+    $('body').removeClass('is-focus is-type');
+  });
+
+  $('.field').on('keydown', function (event) {
+    $('body').addClass('is-type');
+    if ((event.which === 8) && $(this).val() === '') {
+      $('body').removeClass('is-type');
+    }
+  });
+
+});
+let navStatus = false;
+$("#mobile-toggle").click(function () {
+
+  $("#mobile-ul").attr("style", "display:block !important;");
+  if (navStatus) {
+    $(".mobile-navbar").animate({
+      left: "-204px",
+    }, 1500);
+    navStatus = false;
+    document.getElementById("overlay").style.display = "none";
+    $("#nav-button").removeClass();
+    $("#nav-button").addClass("fas fa-bars ml-1")
+  } else {
+    $(".mobile-navbar").animate({
+      left: "0px",
+    }, 1500);
+    navStatus = true;
+    document.getElementById("overlay").style.display = "block";
+    $("#nav-button").removeClass();
+    $("#nav-button").addClass("fa fa-times ml-1")
+
+  }
+});
